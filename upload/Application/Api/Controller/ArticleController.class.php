ee38966fddd499762e0c9c283fe09152e3a7100a
tangtanglove <dai_hang_love@126.com>
2016-04-06 13:38:45 +0800

    修正api文章接口

--- upload/Application/Api/Controller/ArticleController.class.php
+++ upload/Application/Api/Controller/ArticleController.class.php
@@ -6,13 +6,13 @@ namespace Api\Controller;
  */
 class ArticleController extends BaseController{
 
-	/**
-	 * 获取导航栏
-	 */
-	public function navigation_get(){
-		$pid   = I('get.pid',0);
-		$limit = I('get.limit',10);
-		$data  = M('Channel')->where(array('pid'=>$pid,'status'=>1))->limit($limit)->order('sort asc')->select();
+    /**
+     * 获取导航栏
+     */
+    public function navigation_get(){
+        $pid   = I('get.pid',0);
+        $limit = I('get.limit',10);
+        $data  = M('Channel')->where(array('pid'=>$pid,'status'=>1))->limit($limit)->order('sort asc')->select();
 
         //返回数据
         if ($data) {
@@ -23,27 +23,29 @@ class ArticleController extends BaseController{
         } else {
             $this->restError("获取失败！");
         }
-	}
+    }
 
-	/**
-	 * 获取文章列表
-	 */
-	public function lists_get(){
+    /**
+     * 获取文章列表
+     */
+    public function lists_get(){
 
-		$page   = I('get.page',1);
-		$num    = I('get.num',10);
-		$catid  = I('get.catid');
+        $page   = I('get.page',1);
+        $num    = I('get.num',10);
+        //分类标识
+        $name   = I('get.name');
 
-		//搜索关键词
-		$q    = I('get.q');
+        //搜索关键词
+        $q    = I('get.q');
 
-		if (!empty($q)) {
-			$q = mb_convert_encoding($q, "UTF-8", "auto");
-			$where['title'] = array('like','%'.$q.'%');
-		}
-		
-		if (!empty($catid)) {
+        if (!empty($q)) {
+            $q = mb_convert_encoding($q, "UTF-8", "auto");
+            $where['title'] = array('like','%'.$q.'%');
+        }
+        
+        if (!empty($name)) {
 
+            $catid = M('Category')->where(array('name'=>$name))->getField('id');
             $children = D('Category')->getAllChildrenId($catid);
             if (!empty($children)) {
                 //如果有子分类
@@ -59,9 +61,17 @@ class ArticleController extends BaseController{
                 $where['category_id'] = $catid;
             }
 
-		}
+        }
 
-		$data = M('Document')->where($where)->page("$page,$num")->order('sort desc')->select();
+        $where['status'] = 1;
+
+        $data = M('Document')->where($where)->page("$page,$num")->order('level asc,id desc')->select();
+
+        foreach ($data as $key => $value) {
+            $data[$key]['cover']   = get_cover($value['cover_id']);
+            $data[$key]['create_time'] = date('Y-m-d H',$value['create_time']);
+            $data[$key]['update_time'] = date('Y-m-d H',$value['update_time']);
+        }
 
         //返回数据
         if ($data) {
@@ -72,33 +82,33 @@ class ArticleController extends BaseController{
         } else {
             $this->restError("获取失败！");
         }
-	}
-	
+    }
+    
 
-	/**
-	 * 获取文章内容
-	 */
-	public function detail_get(){
+    /**
+     * 获取文章内容
+     */
+    public function detail_get(){
 
-		$id   		= I('get.id');
+        $id         = I('get.id');
 
-		//获得基本数据
-		$data 		= M('Document')->where(array('id'=>$id))->find();
+        //获得基本数据
+        $data       = M('Document')->where(array('id'=>$id))->find();
 
-		//获得内容
-		$content 	= M('DocumentArticle')->where(array('id'=>$id))->getField('content');
+        //获得内容
+        $content    = M('DocumentArticle')->where(array('id'=>$id))->getField('content');
 
-		//内容图片加域名
-		$content 	= $this->replacePicUrl($content,'http://'.$_SERVER['HTTP_HOST'].__ROOT__);
-		
-		/* 更新浏览数 */
-		M('Document')->where(array('id' => $id))->setInc('view');
+        //内容图片加域名
+        $content    = $this->replacePicUrl($content,'http://'.$_SERVER['HTTP_HOST'].__ROOT__);
+        
+        /* 更新浏览数 */
+        M('Document')->where(array('id' => $id))->setInc('view');
         $data['update_time'] = date('Y-m-d H',$data['update_time']);
         $data['username'] = get_username($data['uid']);
         //返回数据
         if ($data) {
-        	//组合数据
-        	$data['content'] = $content;
+            //组合数据
+            $data['content'] = $content;
             $return['status'] = 200;
             $return['info'] = "获取成功！";
             $return['data'] = $data;
@@ -106,10 +116,10 @@ class ArticleController extends BaseController{
         } else {
             $this->restError("获取失败！");
         }
-		
+        
 
-	}
-	
+    }
+    
     /** 
      * 替换内容中的图片 添加域名 
      * @param  string $content 要替换的内容 
@@ -153,13 +163,6 @@ class ArticleController extends BaseController{
             return $content;  
         }  
     }
-	
-	/**
-	 * 获取行政区域列表
-	 */
-	public function region_get(){
-		
-	}
-	
-	
+    
+    
 }
