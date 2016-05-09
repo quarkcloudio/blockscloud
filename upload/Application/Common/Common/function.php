556fbe73344342f5da8a4d70f05d3e28d5b93cb7
tangtanglove <dai_hang_love@126.com>
2016-05-09 10:59:02 +0800

    Update function.php

--- upload/Application/Common/Common/function.php
+++ upload/Application/Common/Common/function.php
@@ -1163,17 +1163,7 @@ function send_sms($phone,$content)
 function sioo_send_sms($phone,$content) {
 
     if(preg_match("/^1[34578]\d{9}$/", $phone)){
-        //执行发短信
-        $msg  = $content;
-        $msg  = mb_convert_encoding($msg,'GBK','utf-8');
-        $uid  = '50011';
-        $auth = '3b0d540feb7cd142f32030e02426820d';
-        $va_url = "http://sms.10690221.com:9011/hy/?uid=".$uid."&auth=".$auth."&mobile=".$phone."&msg=".$msg."&expid=0"; //验证的 url 链接地址  
-        $ch = curl_init($va_url);  
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // 获取数据返回  
-        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true); // 在启用 CURLOPT_RETURNTRANSFER 时候将获取数据返回  
-        $pageContents = curl_exec($ch);
-        curl_close($ch);
+
     }else{
         $this->error('手机号错误！');
     }
