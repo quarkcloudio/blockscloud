11232a1ee137807237e8c187e890e18779ccc17d
tangtanglove <dai_hang_love@126.com>
2017-05-28 19:19:07 +0800

    添加网站icon

--- resources/views/home/detail.blade.php
+++ resources/views/home/detail.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $article->title }} - {{ $website['web_site_title'] }}</title>
+
+        <link rel="shortcut icon" href="/favicon.ico"/>
         <!-- Styles -->
         <link href="/css/bootstrap.css" rel="stylesheet">
         <link href="/css/home.css" rel="stylesheet">
@@ -18,6 +20,11 @@
         <h1>{{ $article->title }} <span style="font-size:12px;">阅读 ({{ $article->view }})</span></h1>
       </div>
       <p class="lead">{!! $article->content !!}</p>
+        <!--打赏-->
+        <div style="height:60px;"></div>
+        <div style="width:100%;text-align:center;">
+            <div id="cyReward" style="margin:0 auto;" role="cylabs" data-use="reward"></div>
+        </div>
         <!--高速版-->
         <div id="SOHUCS" sid="请将此处替换为配置SourceID的语句"></div>
         <script charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/changyan.js" ></script>
@@ -31,5 +38,6 @@
     <!-- Scripts -->
     <script src="/js/vendor/jquery/1.11.3/jquery.min.js"></script>
     <script src="/js/vendor/bootstrap/bootstrap.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="https://changyan.sohu.com/js/changyan.labs.https.js?appid=cyt1wGQrK"></script>
     </body>
 </html>
--- resources/views/home/page.blade.php
+++ resources/views/home/page.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ $page->title }} - {{ $website['web_site_title'] }}</title>
+
+        <link rel="shortcut icon" href="/favicon.ico"/>
         <!-- Styles -->
         <link href="/css/bootstrap.css" rel="stylesheet">
         <link href="/css/home.css" rel="stylesheet">
