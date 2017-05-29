e87f1a193ffe827bc89ce787e74cfd42b64ae16c
tangtanglove <dai_hang_love@126.com>
2017-05-29 09:52:56 +0800

    修正导航bug，调整finder图标目录

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">云市场</a>
+            <a class="navbar-brand" href="#">积木云</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
